73e391201336092bfb9aecf2680ad0010e640c83
Solarint <fear231@gmail.com>
2024-05-29 04:50:27 -0500

    version

--- Plugin/AssemblyInfoClass.cs
+++ Plugin/AssemblyInfoClass.cs
@@ -17,7 +17,7 @@
 
         public const string SAINGUID = "me.sol.sain";
         public const string SAINName = "SAIN";
-        public const string SAINVersion = "2.2.6";
+        public const string SAINVersion = "2.2.6.1";
         public const string SAINPresetVersion = "2.2.6";
 
         public const string SPTGUID = "com.spt-aki.core";
