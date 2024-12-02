46017bbf3b2acceac1c8bf07391ae4f8bb3a7f63
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-12-02 06:58:12 +0100

    Set IsMoving during movement

--- Components/BotComponentSpace/Classes/Mover/SAINSprint.cs
+++ Components/BotComponentSpace/Classes/Mover/SAINSprint.cs
@@ -562,6 +562,7 @@ namespace SAIN.SAINComponent.Classes.Mover
             }
             Player.CharacterController.SetSteerDirection(direction);
             BotOwner.AimingData?.Move(Player.Speed);
+			BotOwner.Mover.IsMoving = true;
             Player.Move(findMoveDirection(direction));
         }
 
