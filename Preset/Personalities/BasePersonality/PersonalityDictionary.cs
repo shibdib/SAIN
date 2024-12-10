67eab3dbde9852b8fc7ddacc6428f0489cbecd14
Solarint <fear231@gmail.com>
2024-12-10 10:55:09 -0600

    Added some names to nickname list

--- Preset/Personalities/BasePersonality/PersonalityDictionary.cs
+++ Preset/Personalities/BasePersonality/PersonalityDictionary.cs
@@ -20,14 +20,16 @@ namespace SAIN.Preset.Personalities
         private class NickNames
         {
             public Dictionary<string, EPersonality> NicknamePersonalityMatches = new Dictionary<string, EPersonality>() {
-            { "solarint", EPersonality.GigaChad},
-            { "chomp", EPersonality.Chad},
-            { "senko", EPersonality.Chad},
-            { "kaeno", EPersonality.Timmy},
-            { "justnu", EPersonality.Timmy},
-            { "ratthew", EPersonality.Rat},
-            { "choccy", EPersonality.Rat},
-        };
+                { "steve", EPersonality.Wreckless},
+                { "solarint", EPersonality.GigaChad},
+                { "lvndmark", EPersonality.SnappingTurtle},
+                { "chomp", EPersonality.Chad},
+                { "senko", EPersonality.Chad},
+                { "kaeno", EPersonality.Timmy},
+                { "justnu", EPersonality.Timmy},
+                { "ratthew", EPersonality.Rat},
+                { "choccy", EPersonality.Rat},
+            };
         }
 
         private static readonly NickNames _nicknames;
